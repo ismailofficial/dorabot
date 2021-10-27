3d44952df1813f9b0ba96d41500571e2c6b18b5e
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-10-27 15:16:27 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -65,7 +65,7 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        // await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | Haruno bot oleh Fadli`).catch(_ => _) // disable jika kamu tidak suka dengan ini
+        // await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} |Dora bot oleh Mr nolep pembuat bot`).catch(_ => _) // disable jika kamu tidak suka dengan ini
         setting.status = new Date() * 1
     }
 
