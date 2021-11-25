cf1880d77dcea49acc6505f9adc8bc1c379761d5
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-11-25 09:58:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'APIKEY',
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.xteam.xyz': '3b710272e0090cdb',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'FVe0JFCdME58YSKmhoDWdRtyuJt',
   'https://api.lolhuman.xyz': 'WajibPatunganKontol!!',
