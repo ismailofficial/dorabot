fe37ecc419fc54f2de3222ea872fba919f6bee66
IG@Ismailreal.id <77217686+ismailofficial@users.noreply.github.com>
2021-11-30 00:09:24 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 function handler(m) {
   m.reply('Chat jika penting, harap tidak spam.')
-  this.sendContact(m.chat, '6289601145546@s.whatsapp.net', 'Dora', m)
+  this.sendContact(m.chat, '6283805279906@s.whatsapp.net', 'Ismail Kurama', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
